678f651c78851ab213bb6540ee8e7767b70baecb
Petr Posvic <petr@posvic.cz>
2016-05-07 11:36:25 +0200

    Show hours in graphs

--- routes/index.js
+++ routes/index.js
@@ -6,21 +6,24 @@ router.get('/', function(req, res, next) {
   db.Measurement
     .findAll()
     .then(function(objs) {
-      var tmps = [], hmds = [];
+      var tmps = {}, hmds = {};
       var max = Math.max(0, objs.length - 200);
       for (var i = max; i < objs.length; i++) {
-        tmps.push(objs[i].temperature);
-        hmds.push(objs[i].humidity);
+        var time = new Date(objs[i].created_at).getTime() / 1000;
+        tmps[time] = objs[i].temperature;
+        hmds[time] = objs[i].humidity;
       }
 
       var plot = require('plotter').plot;
       plot({
         data: { 'Temperature': tmps },
-        filename: 'public/tmp/temperature.png'
+        filename: 'public/tmp/temperature.png',
+        time: 'hours'
       });
       plot({
         data: { 'Humidity': hmds },
-        filename: 'public/tmp/humidity.png'
+        filename: 'public/tmp/humidity.png',
+        time: 'hours'
       });
       res.render('index', { title: 'Smart Home', min:  objs[max].created_at, max: objs[objs.length - 1].created_at});
     });
