3712389ac8e22fbca9c2123638a9437ea1415bc3
Petr Posvic <petr@posvic.cz>
2016-05-24 12:23:37 +0200

    Show office temperature and humidity

--- routes/index.js
+++ routes/index.js
@@ -4,33 +4,76 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
   db.Measurement
-    .findAll()
+    .findAll({
+      limit: 250,
+      order: 'created_at DESC'
+    })
     .then(function(objs) {
-      var tmps = {}, hmds = {};
-      for (var i = Math.max(0, objs.length - 200); i < objs.length; i++) {
-        var time = new Date(objs[i].created_at).getTime() / 1000;
-        tmps[time] = objs[i].temperature;
-        hmds[time] = objs[i].humidity;
+      var tmps = [], hmds = [];
+      tmps['livingroom'] = [];
+      tmps['livingroom_cpu'] = [];
+      hmds['livingroom'] = [];
+      tmps['office'] = [];
+      tmps['office_cpu'] = [];
+      hmds['office'] = [];
+
+      for (var i = 0; i < objs.length; i++) {
+        var time = new Date(objs[i].created_at).getTime() / 1000 + 60 * 60 * 2;
+        if (
+          objs[i].device === 'livingroom' ||
+          objs[i].device === 'livingroom_cpu' ||
+          objs[i].device === 'office' ||
+          objs[i].device === 'office_cpu'
+        ) {
+          tmps[objs[i].device][time] = objs[i].temperature;
+        }
+        if (
+            objs[i].device === 'livingroom' ||
+            objs[i].device === 'office'
+        ) {
+          hmds[objs[i].device][time] = objs[i].humidity;
+        }
       }
 
       var plot = require('plotter').plot;
       plot({
-        data: { 'Temperature': tmps },
+        data: { 'Livingroom': tmps['livingroom'], 'Office': tmps['office'] },
         filename: 'public/tmp/temperature.png',
+        title: 'Temperature',
         time: 'hours'
       });
       plot({
-        data: { 'Humidity': hmds },
+        data: { 'Livingroom': tmps['livingroom_cpu'], 'Office': tmps['office_cpu'] },
+        filename: 'public/tmp/cpu.png',
+        title: 'CPU temperature',
+        time: 'hours'
+      });
+      plot({
+        data: { 'Livingroom': hmds['livingroom'], 'Office': hmds['office'] },
         filename: 'public/tmp/humidity.png',
+        title: 'Humidity',
         time: 'hours'
       });
 
       db.Beacon
         .findAll({
-          order: 'id DESC',
-          limit: 5
+          limit: 50,
+          order: 'created_at DESC'
         })
         .then(function(beacons) {
+
+          var dist = {};
+          for (var i = 0; i < beacons.length; i++) {
+            var time = new Date(beacons[i].created_at).getTime() / 1000 + 60 * 60 * 2;
+            dist[time] = beacons[i].rssi;
+          }
+
+          plot({
+            data: { 'Stroller': dist },
+            filename: 'public/tmp/stroller.png',
+            time: 'hours'
+          });
+
           res.render('index', {
             title: 'Smart Home',
             beacons: beacons
