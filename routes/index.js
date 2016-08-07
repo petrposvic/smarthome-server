23e23acc82077ee9e99b4f90857c1bf598371aea
Petr Posvic <petr@posvic.cz>
2016-08-07 16:54:19 +0200

    Add test GET and POST

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/test', function(req, res) {
+  console.log('GET OK');
+  res.send('GET OK');
+});
+
+router.post('/test', function(req, res) {
+  console.log('POST OK ' + JSON.stringify(req.body));
+  res.send('POST OK');
+});
+
 router.get('/', function(req, res) {
   res.render('index', {
     title: 'Smart Home'
